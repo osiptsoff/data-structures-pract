5373c11916028c400775c6fe0ff5ef5924b6df08
Никита Осипцов <osipczovnikita@gmail.com>
2021-09-14 14:09:28 +0300

    implemented random set generator

--- pract1/SetOnArray/main.cpp
+++ pract1/SetOnArray/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <time.h>
 
 using std::cin;
 using std::cout;
+using std::rand;
+
+
+bool IsIn(char* set, char node)
+{
+	int index;
+
+	for (index = 0; set[index] != node && set[index] != '\0'; ++index);
+	return set[index] == node;
+}
+
+char* GenerateRandomSet(int MaxSize)
+{
+	char* result = new char[MaxSize];
+	char letter;
+	int sizeFact = rand() % MaxSize;
+
+	for (int i = 0; i < sizeFact; ++i)
+	{
+		result[i + 1] = '\0';
+		do
+			letter = rand() % ('z' - 'a' + 1) + 'a';
+		while (IsIn(result, letter));
+		result[i] = letter;
+	}
+	result[sizeFact] = '\0';
+
+	return result;
+}
 
 int main()
 {
@@ -12,35 +42,30 @@ int main()
 	char* result;
 	int counter, resultIndex;
 
+	srand(time(nullptr));
 	for (int i = 0; i < SetsExist; i++)
 	{
-		sets[i] = new char[SetSize];
-		cin >> sets[i];
+		sets[i] = GenerateRandomSet(SetSize);
+		cout << "Set " << (char)('A' + i) << ": " << sets[i] << std::endl;
 	}
 	result = new char[SetSize];
 	resultIndex = 0;
 
 	for (int aIndex = 0; sets[0][aIndex] != '\0'; ++aIndex)
-		for (int bIndex = 0; sets[1][bIndex] != '\0'; ++bIndex)
-			if (sets[0][aIndex] == sets[1][bIndex])
+		if(IsIn(sets[1], sets[0][aIndex]))
+			if(!IsIn(sets[2], sets[0][aIndex]))
 			{
-				for (counter = 0; sets[2][counter] != '\0' && sets[2][counter] != sets[0][aIndex]; ++counter);
-				if (sets[2][counter] == '\0')
-				{
-					result[resultIndex] = sets[0][aIndex];
-					++resultIndex;
-					result[resultIndex] = '\0';
-				}
+				result[resultIndex] = sets[0][aIndex];
+				++resultIndex;
+				result[resultIndex] = '\0';
 			}
+
 	for (int dIndex = 0; sets[3][dIndex] != '\0'; ++dIndex)
-	{
-		for (counter = 0; (result[counter] != '\0') && (result[counter] != sets[3][dIndex]); ++counter);
-		if (result[counter] == '\0')
+		if(!IsIn(result, sets[3][dIndex]))
 		{
 			result[resultIndex] = sets[3][dIndex];
 			++resultIndex;
 		}
-	}
 	result[resultIndex] = '\0';
 
 	cout << "Result: " << result << std::endl;
