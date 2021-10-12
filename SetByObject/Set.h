336af94a8fd513296068ce4b80cd9bb1b94ce00d
Никита Осипцов <osipczovnikita@gmail.com>
2021-10-12 17:08:04 +0300

    set on machine word implemented

--- SetByObject/Set.h
+++ SetByObject/Set.h
@@ -1,2 +1,68 @@
 #pragma once
 
+#include <iostream>
+
+class Set
+{
+	private:
+	    static char universe[];
+		static int universeSize;
+		static int setsExist;
+
+		int word;
+		char tag;
+		int power;
+
+	public:
+		Set(int);
+		Set() : Set(rand() | (rand() << 15)) {}
+		Set(const Set&);
+		//Set(const Set&&) noexcept;
+
+		int GetPower() { return power; }
+		void Print();
+
+		Set operator| (const Set& other) const { return Set(this->word | other.word); };
+		Set operator& (const Set& other) const { return Set(this->word & other.word); };
+		Set operator~ () const { return Set(~(this->word)); };
+		Set operator= (const Set&);
+		//Set operator= (const Set&&) noexcept;
+};
+
+char Set::universe[] = "abcdefghijklmnopqrstuvwxyz";
+int Set::universeSize = strlen(universe);
+int Set::setsExist = 0;
+
+Set::Set(int word)
+{
+	this->word = word;
+	tag = 'A' + setsExist++;
+
+	power = 0;
+	for (int i = 0; i < universeSize; i++)
+		power += ((word & (1 << i)) > 0);
+}
+
+Set::Set(const Set& other)
+{
+	this->word = other.word;
+	this->power = other.power;
+}
+
+void Set::Print()
+{
+	std::cout << "Set " << tag << ": ";
+	std::cout << "[";
+	for (int i = 0; i < universeSize; i++)
+		if ((word & (1 << i)) > 0)
+			std::cout << universe[i];
+	std::cout << "]\n";
+}
+
+Set Set::operator= (const Set& other)
+{
+	this->word = other.word;
+	this->power = other.power;
+	return *this;
+}
+
--- SetByObject/Source.cpp
+++ SetByObject/Source.cpp
@@ -1,3 +1,35 @@
-
+#include <iostream>
+#include <time.h>
 #include "Set.h"
 
+using namespace std;
+
+int main()
+{
+	const int setsExist = 4;
+	const int executions = 1000000;
+
+	Set setA, setB, setC, setD, result;
+	float averagePower = 0;
+
+	srand(time(nullptr));
+	setA = Set();
+	setA.Print();
+	setB = Set();;
+	setB.Print();
+	setC = Set();
+	setC.Print();
+	setD = Set();
+	setD.Print();
+	averagePower = (setA.GetPower() + setB.GetPower() + setC.GetPower() + setD.GetPower()) / 4;
+	
+	time_t start = clock();
+	for(int i = 0; i < executions; i++)
+		result = ((setA & setB) & ~setC) | setD;
+	start = clock() - start;
+
+	cout << "\nTime taken: " << (start / (float)CLOCKS_PER_SEC) << "\nAverage power: " << averagePower << "\n";
+	result.Print();
+
+	return 0;
+}
