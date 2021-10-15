1f3da4b1c4e0a3355efea9911c1de4f3d4418b28
Никита Осипцов <osipczovnikita@gmail.com>
2021-10-15 23:24:09 +0300

    set on array implemented

--- SetByObject/Set.h
+++ SetByObject/Set.h
@@ -5,64 +5,129 @@
 class Set
 {
 	private:
-	    static char universe[];
-		static int universeSize;
+		const static char universe[];
+		const static int universeSize;
 		static int setsExist;
 
-		int word;
+		char* set;
 		char tag;
 		int power;
-
 	public:
-		Set(int);
-		Set() : Set(rand() | (rand() << 15)) {}
+		Set(void*) : set(new char[universeSize + 1]), tag('A' + setsExist++), power(0) { set[0] = '\0'; };
+		Set();
 		Set(const Set&);
-		//Set(const Set&&) noexcept;
+		Set(Set&& other) noexcept : set(other.set), tag(other.tag), power(other.power) { other.set = nullptr; }
+		~Set() { delete[] set; --setsExist; };
 
-		int GetPower() { return power; }
-		void Print();
+		int GetPower() { return power; };
+		void Print() { std::cout << tag <<  ": [" << set << "]\n"; };
 
-		Set operator| (const Set& other) const { return Set(this->word | other.word); };
-		Set operator& (const Set& other) const { return Set(this->word & other.word); };
-		Set operator~ () const { return Set(~(this->word)); };
-		Set operator= (const Set&);
-		//Set operator= (const Set&&) noexcept;
+		Set operator|(const Set&) const;
+		Set operator&(const Set&) const;
+		Set operator~() const;
+		Set operator=(const Set&);
+		Set operator=(Set&&) noexcept;
 };
 
-char Set::universe[] = "abcdefghijklmnopqrstuvwxyz";
-int Set::universeSize = strlen(universe);
+const char Set::universe[] = "abcdefghijklmnopqrstuvwxyz";
+const int Set::universeSize = strlen(universe);
 int Set::setsExist = 0;
 
-Set::Set(int word)
+Set::Set() : Set(nullptr)
 {
-	this->word = word;
-	tag = 'A' + setsExist++;
+	int word = rand() | (rand() << 15);
 
-	power = 0;
-	for (int i = 0; i < universeSize; i++)
-		power += ((word & (1 << i)) > 0);
-}
-
-Set::Set(const Set& other)
-{
-	this->word = other.word;
-	this->power = other.power;
-}
-
-void Set::Print()
-{
-	std::cout << "Set " << tag << ": ";
-	std::cout << "[";
-	for (int i = 0; i < universeSize; i++)
+	for (int i = 0; i < universeSize; ++i)
 		if ((word & (1 << i)) > 0)
-			std::cout << universe[i];
-	std::cout << "]\n";
+		{
+			set[power] = universe[i];
+			++power;
+		}
+
+	set[power] = '\0';
 }
 
-Set Set::operator= (const Set& other)
+Set::Set(const Set& other) : set(new char[universeSize + 1]), tag('A' + setsExist++), power(other.power)
 {
-	this->word = other.word;
-	this->power = other.power;
+	for (int i = 0; this->set[i] = other.set[i]; ++i);
+	set[power] = '\0';
+}
+
+Set Set::operator|(const Set &other) const
+{
+	Set result = Set(*this);
+
+	for (int otherIndex = 0; other.set[otherIndex] != '\0'; ++otherIndex)
+	{
+		int thisIndex;
+		for (thisIndex = 0; this->set[thisIndex] != '\0' && this->set[thisIndex] != other.set[otherIndex]; ++thisIndex);
+		if (this->set[thisIndex] == '\0')
+		{
+			result.set[result.power] = other.set[otherIndex];
+			++result.power;
+		}
+	}
+	result.set[result.power] = '\0';
+			
+	return result;
+}
+
+Set Set::operator&(const Set& other) const
+{
+	Set result = Set(nullptr);
+
+	for (int otherIndex = 0; other.set[otherIndex] != '\0'; ++otherIndex)
+	{
+		int thisIndex;
+		for (thisIndex = 0; this->set[thisIndex] != '\0' && this->set[thisIndex] != other.set[otherIndex]; ++thisIndex);
+		if (this->set[thisIndex] != '\0')
+		{
+			result.set[result.power] = other.set[otherIndex];
+			++result.power;
+		}
+	}
+	result.set[result.power] = '\0';
+
+	return result;
+}
+
+Set Set::operator~() const
+{
+	Set result = Set(nullptr);
+
+	for (int universeIndex = 0; universeIndex < universeSize; ++universeIndex)
+	{
+		int thisIndex;
+		for (thisIndex = 0; this->set[thisIndex] != '\0' && this->set[thisIndex] != universe[universeIndex]; ++thisIndex);
+		if (this->set[thisIndex] == '\0')
+		{
+			result.set[result.power] = universe[universeIndex];
+			++result.power;
+		}
+	}
+	result.set[result.power] = '\0';
+
+	return result;
+}
+
+Set Set::operator=(const Set& other)
+{
+	if (this != &other)
+	{
+		this->power = other.power;
+		for (int i = 0; this->set[i] = other.set[i]; ++i);
+		set[power] = '\0';
+	}
 	return *this;
 }
 
+Set Set::operator=(Set&& other) noexcept
+{
+	if (this != &other)
+	{
+		this->power = other.power;
+		this->set = other.set;
+		other.set = nullptr;
+	}
+	return *this;
+}
